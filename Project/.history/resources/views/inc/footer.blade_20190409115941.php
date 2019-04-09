be32c56daf4211733aeb2c782fd94ec0a530254f
Bashar <basharbasil1234@outlook.com>
2019-04-09 12:15:15 +0200

    -a

---
+++ Project/.history/resources/views/inc/footer.blade_20190409115941.php
@@ -0,0 +1,25 @@
+
+@section('extra-css')
+    <link href="css/footer.css" rel="stylesheet">
+@endsection
+
+<footer class="footer-distributed">
+	<div class="container">
+		<div class="footer-right">
+			<p class="Color" >
+				Adres Gegevens:<br><br>
+				Plaats:  Leusden
+				<br>
+				Straat:  Bavoortseweg 27a
+				<br>
+				Postcode:  3830 AB
+				<br>
+				Telefoonnummer:  06-28946024
+			</p>
+		</div>
+		<div class="footer-left">
+			
+			<p>@Copyright GV-Impala 2019 &copy;</p>
+		</div>
+	</div>
+</footer>
---
+++ Project/.history/resources/views/inc/header.blade_20190409110355.php
@@ -0,0 +1,56 @@
+<header>
+    <div class="gray-balk">
+        <div class="container">
+            @guest
+                <div class="gray-balk-text">
+                    <a class="credentials float-left" href="#login">Login |</a>
+                    <a class="credentials float-left" href="#register">Nog geen account? registreer dan <b>hier!</b></a>   
+                    <a class="credentials " href="#contact">contact</a>   
+                </div>
+                @else
+                <div class="loged-in">
+                    <a class="float-right  credentials" href="{{ route('logout') }}"
+                        style="text-decoration: none"
+                        onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>    
+                    <p class="credentials">Welkom, {{ Auth::user()->name }}</p> <span class="caret"></span>
+                </div>
+            @endguest
+        </div>
+    </div>
+    <nav>
+        <div class="logo">
+            <div class="container">
+                <img class="image" src="https://gvimpala.nl/wp-content/uploads/Impala-14-1024x293-1-e1519314257687.jpg">
+                <img class="image-mobile" src="https://gvimpala.nl/wp-content/uploads/LustrumLogo2018-3-e1519314202794.jpg" >
+                <i class="fa fa-bars fa-2x" id="hamburger"></i>
+            </div>
+        </div>
+        <div class="nav-links" id="menu">
+            <ul class="nav-links-list">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#calendar">Kalender</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
+@section('extra-js')
+    <script>
+        $('#hamburger').click(() => {
+            $('#menu').toggleClass('show')
+            $('#hamburger').toggleClass('show')
+        })
+        $('.nav-links-list li a').click(() => {
+            $('#hamburger').removeClass('show')
+            $('#menu').removeClass('show')
+        })
+
+    </script>
+@endsection
+
